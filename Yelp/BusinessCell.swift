ac52770ebf60bd851998154d2066ccafaa11f795
sywxh2012 <xinghan.wang@wustl.edu>
2016-02-14 22:40:12 -0600

    Submission of Yelp

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,66 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Xinghan Wang on 2/11/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+
+    @IBOutlet weak var thumbImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    @IBOutlet weak var ratingImageView: UIImageView!
+   
+    
+    @IBOutlet weak var reviewsCountLabel: UILabel!
+
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    @IBOutlet weak var categoriesLabel: UILabel!
+    
+    var business: Business! {
+        didSet{
+            nameLabel.text = business.name
+        thumbImageView.setImageWithURL(business.imageURL!)
+            categoriesLabel.text = business.categories
+            addressLabel.text = business.address
+            reviewsCountLabel.text = "\(business.reviewCount!) Reviews"
+            ratingImageView.setImageWithURL(business.ratingImageURL!)
+            distanceLabel.text = business.distance
+            
+            
+            
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        thumbImageView.layer.cornerRadius = 3
+        thumbImageView.clipsToBounds = true
+        
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+        // Initialization code
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
